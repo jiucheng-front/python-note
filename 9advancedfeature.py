6aa027b11061814db58838ce08fd5ba5ace010e0
JianFei <wang.jf@burgeon.cn>
2026-06-12 17:20:39 +0800

    列表生成式

--- 9advancedfeature.py
+++ 9advancedfeature.py
@@ -97,6 +97,4 @@ print('arr2:', arr2)
 
 L1 = ['Hello', 'World', 18, 'Apple', None]
 L2 = [s.lower() for s in L1 if isinstance(s,str) == True]
-L3 = [s if isinstance(s,str) == True else s s.lower() for s in L1]
 print('L2:', L2)
-print('L3:', L3)
