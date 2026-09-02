99ab078dc8361d5f56cfc75651688b099dac0feb
王建霏 <wang.jf@burgeon.cn>
2026-09-02 15:41:21 +0800

    if条件判断

---
+++ 3条件判断.py
@@ -0,0 +1,11 @@
+
+
+import random
+num1 = random.randint(1, 10)
+
+if num1 > 8:
+    print(f'数字是{num1}大于8')
+elif num1 > 5:
+    print(f'数字是{num1}大于5')
+else:
+    print(f"数字是{num1}")
---
+++ 7函数input.py
@@ -0,0 +1,13 @@
+
+
+num = 0
+while num < 10:
+    num += 1
+    print(f"当前是第{num}行")
+
+age = int(input("请输入年龄"))
+
+if age >= 18:
+    print('成年了')
+else:
+    print("未成年")
