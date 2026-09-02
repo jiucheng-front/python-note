7b530dccc932d44026e25b22ae1abf139d85c63d
王建霏 <wang.jf@burgeon.cn>
2026-09-02 10:51:44 +0800

    字符串占位符%s%d%f

---
+++ 2字符串拼接.py
@@ -0,0 +1,15 @@
+
+"""
+格式化拼接符号
+%s  字符串占位符
+%d  整数占位符
+%f  浮点数占位符
+"""
+def my_func():
+    # user_name = input("Enter your name: ")
+    user_name = "娃哈哈"
+    user_age = 18
+    user_height = 1.65
+    print("大家好，我是%s, 今年是%d岁，身高是%f" % (user_name, user_age, user_height))
+
+my_func()
