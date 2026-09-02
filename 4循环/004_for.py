bebcf27092e0d69637bb7ea9730d82c618ac4dac
王建霏 <wang.jf@burgeon.cn>
2026-09-02 17:23:17 +0800

    range1

--- 4循环/004_for.py
+++ 4循环/004_for.py
@@ -3,3 +3,14 @@
 names = ['Michael', 'Bob', 'Tracy']
 for name in names:
     print(name)
+
+
+arr1 = list(range(10))
+for num in arr1:
+    print(num)
+
+for x in list(range(5, 10)):
+    print(f"当前是{x}")
+
+for x in list(range(5, 10, 2)):
+    print(f"当前是---{x}")
