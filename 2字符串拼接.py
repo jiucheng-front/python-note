3fc68d3958d2a5fea81f9fa694fcd24e08abc71a
王建霏 <wang.jf@burgeon.cn>
2026-09-02 11:19:43 +0800

    数字占位符的精度控制

--- 2字符串拼接.py
+++ 2字符串拼接.py
@@ -4,6 +4,10 @@
 %s  字符串占位符
 %d  整数占位符
 %f  浮点数占位符
+
+数字进度控制，会四舍五入
+%m.n
+%.nf 小数点后几位
 """
 def my_func():
     # user_name = input("Enter your name: ")
@@ -11,5 +15,6 @@ def my_func():
     user_age = 18
     user_height = 1.65
     print("大家好，我是%s, 今年是%d岁，身高是%f" % (user_name, user_age, user_height))
+    print("大家好，我是%s, 今年是%3d岁，身高是%.3f" % (user_name, user_age, user_height))
 
 my_func()
