ee503663f8ea281abb5caba054a94cbe3671f1b5
王建霏 <444128852@qq.com>
2026-09-03 11:24:03 +0800

    for循环的乘法口诀2

--- 4循环/005_for.py
+++ 4循环/005_for.py
@@ -1,8 +1,9 @@
 """
-for 乘法表
+for 循环输出乘法表
 """
-
+# 横向行数是1-9行，不包括10
 for row in range(1, 10):
+    # 当前行的列数从1到当前行*行
     for cell in range(1, row + 1):
         print(f"{cell} * {row} = {cell * row}\t", end="")
     # 尾部换行一下
